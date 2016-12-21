76e1e904f1f27987385bbd8384c7f00f943893b3
lizhenbin08@sina.com <125906862mac>
2016-12-21 20:20:13 +0800

    test

--- springboot-learn/src/main/java/org/lzbruby/service/UserServiceImpl.java
+++ springboot-learn/src/main/java/org/lzbruby/service/UserServiceImpl.java
@@ -1,13 +1,11 @@
 package org.lzbruby.service;
 
-import org.lzbruby.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
